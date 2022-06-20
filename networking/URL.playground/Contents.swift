76cd891cd4839da236a3e39d63147134e5a3758b
Tin Tran <tinhardware@gmail.com>
2022-06-20 23:55:30 +0700

    Add URL swift code

--- networking/URL.playground/Contents.swift
+++ networking/URL.playground/Contents.swift
@@ -1,42 +1,3 @@
-## Networking Terms
-* Data: (Usually in the raw form) anything that is sent over the network.
-* Client:
-* Server:
-* HTTP: Hypertext Transfer Protocol
-* HTTPS: HTTP Secure,
-
-## Requests and Responses
-A HTTP response consists of a body and a status code.
-
-* Body: Content of a response (such as the HTML that makes up a webpage
-* Status code: 3 digit number signaling the type of response (success, failure, etc.)
-
-
-## Status code
-
-| **Status Code** | **Short Description** | **Category** |
-|-----------------|-----------------------|--------------|
-| 100             | Continue              | Information  |
-| 200             | OK                    | Success      |
-| 301             | Moved Permanently     | Redirection  |
-| 403             | Forbidden             | Client Error |
-| 404             | Not Found             | Client Error |
-| 418             | I'm a teapot          | Client Error |
-| 500             | Internal Server Error | Server Error |
-
-## HTTP Verbs
-
-| **Verb** | **CRUD Operation** |
-|----------|--------------------|
-| GET      | Read               |
-| POST     | Create             |
-| PUT      | Update (replace)   |
-| PATCH    | Update (modify)    |
-| DELETE   | Delete             |
-
-### Swift URL
-
-```swift
 import Foundation
 
 let urlString = "https://itunes.apple.com/us/app/udacity/id819700933?mt=8"
@@ -62,8 +23,6 @@ simpleURL?.appendPathComponent("app")
 simpleURL?.appendPathComponent("udacity")
 simpleURL?.appendPathComponent("id819700933")
 print("simpleURL: \(simpleURL!)" ) // https://itunes.apple.com/us/app/udacity/id819700933
-// but that won't work with queries and fragments
 simpleURL?.appendPathComponent("?mt=8")
+// but that won't work with queries and fragments
 print("simpleURL: \(simpleURL!)" ) // https://itunes.apple.com/us/app/udacity/id819700933/%3Fmt=8
-
-```
