e42f2624c824a004b6df2287985b829382973b64
Tin Tran <tinhardware@gmail.com>
2022-06-21 15:37:55 +0700

    TheMovieManager: update storyboard

---
+++ networking/TheMovieManager/TheMovieManager/Controller/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TheMovieManager
+//
+//  Created by Tin Tran on 21/06/2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var emailTextField: LoginTextField!
+    @IBOutlet weak var passwordTextField: LoginTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func loginTapped(_ sender: Any) {
+    }
+    
+    @IBAction func loginViaWebsiteTapped(_ sender: Any) {
+    }
+}
+
